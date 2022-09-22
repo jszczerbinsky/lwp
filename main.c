e1f1fb9e307390a6e3c2c859153b19b53a0a051a
jszczerbinsky <jszczerbinsky@gmail.com>
2022-09-22 21:22:26 +0200

    Windows support

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <X11/Xlib.h>
 #include <SDL2/SDL.h>
-#include <linux/limits.h>
+#ifdef __WIN32
+	#include <windows.h>
+	#include <stdio.h>
+	#include <SDL2/SDL_syswm.h>
+#else
+	#include <X11/Xlib.h>
+	#include <linux/limits.h>
+#endif
 
 typedef struct
 {
@@ -8,22 +14,73 @@ typedef struct
 	SDL_Texture *buffTex;
 } Instance;
 
-void init(Display **display, SDL_Window **window, SDL_Renderer **renderer)
-{
-	*display = XOpenDisplay(NULL);	
-	Window rootWindow = RootWindow(*display, DefaultScreen(*display));
+#ifdef __WIN32
+	HWND iconWorkerw;
+	BOOL CALLBACK getIconWorkerw(HWND hWnd, LPARAM lParam)
+	{
+		char buff[10];
+		GetClassName(hWnd, buff, 10);
+		
+		if(strcmp(buff, "WorkerW") == 0)
+		{
+			HWND defView = FindWindowEx(hWnd, NULL, "SHELLDLL_DefView", NULL);
+			if(defView)
+			{
+				iconWorkerw = hWnd;
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
 
+	void init(SDL_Window **window, SDL_Renderer **renderer)
+#else
+	void init(Display **display, SDL_Window **window, SDL_Renderer **renderer)
+#endif
+{
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
 		SDL_Log("%s", SDL_GetError());
-
-	*window = SDL_CreateWindowFrom((void*)rootWindow);	
-	if(*window == NULL)
-		SDL_Log("%s", SDL_GetError());
-
+	
+	#ifdef __WIN32
+		*window = SDL_CreateWindow("Parallax wallpaper", 0, 0, 0, 0, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
+		if(*window == NULL)
+			SDL_Log("%s", SDL_GetError());
+		
+		SDL_SysWMinfo sysWmInfo;
+		SDL_VERSION(&sysWmInfo.version)
+		SDL_GetWindowWMInfo(*window, &sysWmInfo);
+		HWND hWindow = sysWmInfo.info.win.window;
+		
+		HWND progman = FindWindow("Progman", NULL);
+		iconWorkerw = progman;
+		SendMessageTimeout(progman, 0x052C, NULL, NULL, SMTO_NORMAL, 1000, NULL);
+		if(!FindWindowEx(progman, NULL, "SHELLDLL_DefView", NULL))
+			EnumWindows(getIconWorkerw, NULL);
+		
+		HWND wallpaperWorkerw = GetWindow(iconWorkerw, GW_HWNDNEXT);
+		SetParent(hWindow, wallpaperWorkerw);
+		SetWindowLongPtr(hWindow, GWL_EXSTYLE, WS_EX_LEFT | WS_EX_LTRREADING | WS_EX_RIGHTSCROLLBAR | WS_EX_NOACTIVATE);
+		SetWindowLongPtr(hWindow, GWL_STYLE, WS_CHILDWINDOW | WS_VISIBLE);
+		
+		SetWindowPos(hWindow, NULL,
+			GetSystemMetrics(SM_XVIRTUALSCREEN),
+			GetSystemMetrics(SM_YVIRTUALSCREEN),
+			GetSystemMetrics(SM_CXVIRTUALSCREEN),
+			GetSystemMetrics(SM_CYVIRTUALSCREEN),
+			SWP_NOZORDER | SWP_NOACTIVATE | SWP_SHOWWINDOW
+		);
+	#else
+		*display = XOpenDisplay(NULL);	
+		Window rootWindow = RootWindow(*display, DefaultScreen(*display));
+	
+		*window = SDL_CreateWindowFrom((void*)rootWindow);	
+		if(*window == NULL)
+			SDL_Log("%s", SDL_GetError());
+	#endif
+	
 	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if(*renderer == NULL)
 		SDL_Log("%s", SDL_GetError());
-
 }
 
 void loadTextures(SDL_Renderer *renderer, SDL_Texture **tex, char *dir, int count, int *srcWidth, int *srcHeight)
@@ -63,14 +120,19 @@ int main(int argc, char *argv[])
 	char *dir = argv[2];
 
 	int srcWidth, srcHeight;
-
-	Display *display;
+	
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
 	SDL_Texture *tex[count];
 
-	init(&display, &window, &renderer);
+	#ifdef __WIN32
+		init(&window, &renderer);
+	#else
+		Display *display;
+		init(&display, &window, &renderer);
+	#endif
+	
 	loadTextures(renderer, tex, dir, count, &srcWidth, &srcHeight);
 
 	for(int i = 0; i < instancesCount; i++)
@@ -95,13 +157,21 @@ int main(int argc, char *argv[])
 	while(!quit)
 	{
 		int mx;
+		
+		#ifdef __WIN32
+			POINT mPos;
+			GetCursorPos(&mPos);
+			mx = mPos.x;
+		#endif
 
 		while(SDL_PollEvent(&event))
 		{
 			if(event.type == SDL_QUIT)
 				quit = 1;
-			else if(event.type == SDL_MOUSEMOTION)
-				mx = event.motion.x;
+			#ifndef __WIN32
+				else if(event.type == SDL_MOUSEMOTION)
+					mx = event.motion.x;
+			#endif
 		}
 
 		for(int u = 0; u < instancesCount; u++)
@@ -151,7 +221,9 @@ int main(int argc, char *argv[])
 		SDL_DestroyTexture(tex[i]);
 	for(int i = 0; i < instancesCount; i++)
 		SDL_DestroyTexture(instances[i].buffTex);
-	XCloseDisplay(display);
+	#ifndef __WIN32
+		XCloseDisplay(display);
+	#endif
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
