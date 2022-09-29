ca37560fa1990d895d2043f65756f5a5ef8bc4e0
jszczerbinsky <jszczerbinsky@gmail.com>
2022-09-29 13:30:30 +0200

    Added icon

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-gcc -mwindows main.c parser.c debug.c window.c -lmingw32 -lSDL2main -lSDL2 -o lwp.exe
+windres resource.rc -O coff -o resource.res
+gcc -mwindows main.c parser.c debug.c window.c resource.res -lmingw32 -lSDL2main -lSDL2 -o lwp.exe
