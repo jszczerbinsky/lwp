9561b331d65279c3ae0b749b788ddc9d4d63be35
jszczerbinsky <jszczerbinsky@gmail.com>
2023-02-11 16:03:36 +0100

    Fixed build script

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 mkdir data
 windres resource.rc -O coff -o resource.res
 gcc -mwindows main.c parser.c debug.c window.c resource.res -lmingw32 -lSDL2main -lSDL2 -o data\lwp.exe
-xcopy wallpapers data\wallpapers \E
+xcopy wallpapers data\wallpapers\ /E /Y
 copy defaultWin.cfg data
 copy LICENSE data\LICENSE.txt
 echo.
