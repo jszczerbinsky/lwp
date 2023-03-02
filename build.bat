e0c3072a76d01737f333a3aefe19d94408d6cca2
Jakub Szczerbiński <chicknman4236@gmail.com>
2023-03-02 23:57:45 +0100

    Set process dpi awarness mode

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 if exist data rmdir /S /Q data
 mkdir data || goto err
 windres resource.rc -O coff -o resource.res || goto err
-gcc -mwindows main.c wallpaper.c parser.c debug.c window.c resource.res -lmingw32 -lSDL2main -lSDL2 -o data\lwp.exe || goto err
+gcc -mwindows main.c wallpaper.c parser.c debug.c window.c resource.res -lmingw32 -lshcore -lSDL2main -lSDL2 -o data\lwp.exe || goto err
 xcopy wallpapers data\wallpapers\ /E /Y || goto err
 copy uninstall.bat data || goto err
 copy defaultWin.cfg data || goto err
